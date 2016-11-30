4a7919f4b7f33fd1c465716a67152f4533fd97ac
brian lynch <brian.lynch4444@gmail.com>
2016-11-30 17:40:58 -0500

    remove comments

--- app/src/main/java/edu/bsu/cs222/pigeonchat/LoginActivity.java
+++ app/src/main/java/edu/bsu/cs222/pigeonchat/LoginActivity.java
@@ -34,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        authenticator = firebaseConnector.getAuthenticator(); //FirebaseAuth.getInstance();
+        authenticator = firebaseConnector.getAuthenticator();
         setup();
     }
 
