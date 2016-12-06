2c5ea01d93a331ea52543e68bcd2722a8a21e5aa
Joel Levy <jdlevy@bsu.edu>
2016-12-06 18:11:33 -0500

    Refactor PasswordChecker

--- app/src/main/java/edu/bsu/cs222/pigeonchat/PasswordChecker.java
+++ app/src/main/java/edu/bsu/cs222/pigeonchat/PasswordChecker.java
@@ -2,32 +2,67 @@ package edu.bsu.cs222.pigeonchat;
 
 public class PasswordChecker {
     private String password;
-    private String confirmPassword;
+    private final String specialCharacters = "!@#$%^&*()";
+    private final String numberCharacters = "1234567890";
 
     public PasswordChecker(String password) {
         this.password = password;
     }
 
     public boolean isValid(String confirmPassword){
-        this.confirmPassword = confirmPassword;
-        return notEmpty() && comparePasswords();
+        return notEmpty() && comparePasswords(confirmPassword);
     }
 
     private boolean notEmpty() {
         return !(password.equals(""));
     }
 
-    public boolean comparePasswords(){
+    public boolean comparePasswords(String confirmPassword){
         return password.equals(confirmPassword);
     }
 
-/**
     public String checkStrength(){
-        if password.length() < 8 ||
-        //if password only contains characters, and is less than 8 charactesr return weak
-        //if password contains at least one number and the length is equal to or greater than 8, return medium
-        //if password contains a symbol, a number, and is greater than 8, return strong
-        return "words";
+
+        if(isStrong()){
+            return "strong";
+        }
+        else if(isMedium()){
+            return "medium";
+        }
+        else{
+            return "weak";
+        }
     }
- **/
+
+    public boolean isStrong(){
+        return password.length() >= 8 && this.countCharactersFromString(numberCharacters) > 0 && this.countCharactersFromString(specialCharacters) > 0;
+    }
+
+    public boolean isMedium(){
+        return password.length() >= 8 && this.countCharactersFromString(numberCharacters) > 0;
+    }
+
+    public int numberCharacterCount(){
+        return countCharactersFromString(numberCharacters);
+    }
+
+     public int specialCharacterCount(){
+         return countCharactersFromString(specialCharacters);
+     }
+
+    public int countCharactersFromString(String characterList){
+        String currentListCharacter;
+        String currentPasswordCharacter;
+        int counter = 0;
+        for(int i=0;i<characterList.length();i++){
+            currentListCharacter = String.valueOf(characterList.charAt(i));
+            for(int j=0;j<password.length();j++){
+                currentPasswordCharacter = String.valueOf(password.charAt(j));
+                if(currentPasswordCharacter.equals(currentListCharacter)){
+                    counter += 1;
+                }
+            }
+        }
+        return counter;
+    }
 }
--- app/src/test/java/edu/bsu/cs222/pigeonchat/PasswordCheckerTest.java
+++ app/src/test/java/edu/bsu/cs222/pigeonchat/PasswordCheckerTest.java
@@ -6,15 +6,50 @@ import static org.junit.Assert.assertEquals;
 
 public class PasswordCheckerTest {
     private PasswordChecker checker;
-    @Test
-    public void samePasswords(){
-        checker = new PasswordChecker("password","password");
-        assertEquals(true, checker.comparePasswords());
+
+    @Test public void zeroNumberCount(){
+        checker = new PasswordChecker("password");
+        assertEquals(0, checker.numberCharacterCount());
+    }
+    @Test public void oneNumberCount(){
+        checker = new PasswordChecker("passw0rd");
+        assertEquals(1, checker.numberCharacterCount());
+    }
+
+    @Test public void fiveNumberCount(){
+        checker = new PasswordChecker("p2as1sw0r6d7");
+        assertEquals(5, checker.numberCharacterCount());
+    }
+    @Test public void oneSpecialCharCount(){
+        checker = new PasswordChecker("p@sswrd");
+        assertEquals(1, checker.specialCharacterCount());
+    }
+
+    @Test public void fiveSpecialCharCount(){
+        checker = new PasswordChecker("pa%s)s(wo^rd!");
+        assertEquals(5, checker.specialCharacterCount());
+    }
+
+    @Test public void zeroSpecialCharCount(){
+        checker = new PasswordChecker("password");
+        assertEquals(0, checker.specialCharacterCount());
     }
 
     @Test
-    public void differentPasswords(){
-        checker = new PasswordChecker("joelsucks","password");
-        assertEquals(false, checker.comparePasswords());
+    public void testWeakPassword() {
+        checker = new PasswordChecker("password");
+        assertEquals("weak", checker.checkStrength());
+    }
+
+    @Test
+    public void testMediumPassword(){
+        checker = new PasswordChecker("passw0rd");
+        assertEquals("medium", checker.checkStrength());
+    }
+
+    @Test
+    public void testStrongPassword(){
+        checker = new PasswordChecker("p@ssw0rd");
+        assertEquals("strong", checker.checkStrength());
     }
 }
